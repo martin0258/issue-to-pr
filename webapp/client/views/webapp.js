4cdbfb8ddac31a59e3424f775107ce443af7da68
Martin Ku <martin79831@gmail.com>
2014-05-03 16:24:00 +0800

    UI Redesign: use issue URL and add examples.

--- webapp/client/views/webapp.js
+++ webapp/client/views/webapp.js
@@ -4,11 +4,25 @@ Template.issue_to_pr.events({
     // Get inputs from users
     var currentUser = Meteor.user();
     var token = currentUser.services.github.accessToken;
-    var user = $('#user').val();
-    var repo = $('#repo').val();
-    var issue_num = $('#issue-number').val();
+    var issue_url = $('#issue-url').val();
     var base = $('#base').val();
     var head = $('#head').val();
+
+    // Example: https://github.com/SkysourceRepo/ER-Bonus-System/issues/777
+    var domain = "https://github.com/";
+    var split_by_domain = issue_url.split(domain);
+    if (split_by_domain.length != 2) {
+      toastr.error('Conversion failed. Issue URL is wrong.');
+      return;
+    }
+    var user_repo_num = split_by_domain[1].split('/');
+    if (user_repo_num.length != 4) {
+      toastr.error('Conversion failed. Issue URL is wrong.');
+      return;
+    }
+    var user = user_repo_num[0];
+    var repo = user_repo_num[1];
+    var issue_num = user_repo_num[3];
 
     Meteor.call('convert_issue_to_pr',
                 token, user, repo,
